4ec3dc5e9179edf3c29b7c9687c4681f06bf2901
mjozefow <marcinjoz87@gmail.com>
2018-06-03 15:36:10 +0200

    Split resources. Small Refactor of session factory instantiation

--- src/main/java/infiniteskills/model/HibernateUtil.java
+++ src/main/java/infiniteskills/model/HibernateUtil.java
@@ -5,17 +5,24 @@ import org.hibernate.SessionFactory;
 import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 import org.hibernate.cfg.Configuration;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
 public class HibernateUtil {
-    private static final SessionFactory sf = buildSessionFactoryByXMl();
+    private static final SessionFactory sf = buildSessionFactoryByPropertiesFile();
+
+    public static SessionFactory getSf() {
+        return sf;
+    }
 
     private static SessionFactory buildSessionFactoryByPropertiesFile() {
-        try
-        {
+        try {
             Configuration cfg = new Configuration();
+            cfg.addProperties(getProperties());
             cfg.addAnnotatedClass(User.class);
-            return cfg.buildSessionFactory(new StandardServiceRegistryBuilder().build());
-        }
-        catch(Exception e){
+            return cfg.buildSessionFactory(new StandardServiceRegistryBuilder().applySettings(cfg.getProperties()).build());
+        } catch (Exception e) {
             e.printStackTrace();
             throw new RuntimeException();
         }
@@ -24,6 +31,8 @@ public class HibernateUtil {
     private static SessionFactory buildSessionFactoryByXMl() {
         try {
             Configuration cfg = new Configuration();
+            cfg.configure("./xml/hibernate.cfg.xml");
+            cfg.addAnnotatedClass(User.class);
             return cfg.buildSessionFactory(new StandardServiceRegistryBuilder().applySettings(cfg.getProperties()).build());
         } catch (Exception e) {
             e.printStackTrace();
@@ -31,7 +40,11 @@ public class HibernateUtil {
         }
     }
 
-    public static SessionFactory getSf() {
-        return sf;
+    private static Properties getProperties() throws IOException {
+        Properties prop = new Properties();
+        InputStream in = HibernateUtil.class.getResourceAsStream("/prop/hibernate.properties");
+        prop.load(in);
+        in.close();
+        return prop;
     }
 }
